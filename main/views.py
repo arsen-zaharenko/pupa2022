c1df51e55a07b0e61e15f656d73172c8b13ba4d5
arsen-zaharenko <59254411+arsen-zaharenko@users.noreply.github.com>
2022-01-30 17:36:34 +0300

    Update views.py

--- main/views.py
+++ main/views.py
@@ -1,28 +1,21 @@
 from django.shortcuts import render
 import json, psycopg2
 from psycopg2 import Error
+from main.models import InputList
 
 def index(request):
     return render(request, 'main/index.html')
 
 def data(request):
     global connection, cursor
-    inputs = []
-    select_query = "SELECT * FROM inputs;"
-    create_table_query = "CREATE TABLE inputs (ID SERIAL PRIMARY KEY, data JSONB);"
+    select_query = "SELECT * FROM main_inputlist;"
     try:
         connection, cursor = connect_to_db()
 
         cursor.execute(select_query)
         inputs = cursor.fetchall()
     except (Exception, Error) as error:
-        if "does not exist" in str(error):
-            connection, cursor = connect_to_db()
-
-            cursor.execute(create_table_query)
-            connection.commit()
-        else:
-            return render(request, 'main/error.html', {'error': error})
+        return render(request, 'main/error.html', {'error': error})
     finally:
         if connection:
             cursor.close()
@@ -31,7 +24,6 @@ def data(request):
     return render(request, 'main/data.html', {'inputs': inputs})
 
 def submit(request):
-    global connection, cursor
     if request.method == 'POST':
         data = dict()
         inputs = request.POST.getlist('input', [])
@@ -39,31 +31,10 @@ def submit(request):
         for id, value in enumerate(inputs):
             data[id] = value
 
-        create_table_query = "CREATE TABLE inputs (ID SERIAL PRIMARY KEY, data JSONB);"
-        insert_query = f"INSERT INTO inputs (DATA) VALUES ('{json.dumps(data)}');"
+        input_list = InputList(data=json.dumps(data))
+        input_list.save()
 
-        try:
-            connection, cursor = connect_to_db()
-
-            cursor.execute(create_table_query)
-            connection.commit()
-
-            cursor.execute(insert_query)
-            connection.commit()
-        except (Exception, Error) as error:
-            if "already exists" in str(error):
-                connection, cursor = connect_to_db()
-
-                cursor.execute(insert_query)
-                connection.commit()
-            else:
-                return render(request, 'main/error.html', {'error': error})
-        finally:
-            if connection:
-                cursor.close()
-                connection.close()
-
-        return render(request, 'main/submit.html', {'inputs': data})
+        return render(request, 'main/submit.html', {'inputs': input_list.data})
     return render(request, 'main/error.html')
 
 def connect_to_db():
